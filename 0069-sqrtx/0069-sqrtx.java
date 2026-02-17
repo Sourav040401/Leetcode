3c78e6a6be7c2a0928bf449bc63cc90c136db373
SOURAV KUMAR SHARMA <145861981+Sourav040401@users.noreply.github.com>
2026-02-17 17:12:36 +0530

    Time: 22 ms (7.12%), Space: 42.6 MB (80.16%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int mySqrt(int x) {
+        int result =0;
+        for(int i=0;i<=x;i++){
+            if((long)i*i<=x){
+                result=i;
+            }else
+            break;
+        }
+        return result;
+    }
+}
